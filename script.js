78004eabd01177446ca2fcf4c9be2b091e98e5d2
floryid <nofriintraco@gmail.com>
2024-09-27 23:42:49 +0900

    first commit

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-// Toggle menu on mobile
-const openMenu = document.getElementById('open-menu');
-const closeMenu = document.getElementById('close-menu');
-const navLinks = document.getElementById('nav-links');
 
-// Open the menu when the bar icon is clicked
-openMenu.addEventListener('click', () => {
-    navLinks.classList.add('show');
-});
-
-// Close the menu when the X icon is clicked
-closeMenu.addEventListener('click', () => {
-    navLinks.classList.remove('show');
-});
-
-// Close menu when a link is clicked
-const navItems = document.querySelectorAll('#nav-links li a');
-navItems.forEach(item => {
-    item.addEventListener('click', () => {
-        navLinks.classList.remove('show');
-    });
-});
+  // Google search function
+  function searchGoogle() {
+    const query = prompt("Enter search query:");
+    if (query) {
+      window.open(`https://www.google.com/search?q=${query}`, "_blank");
+    }
+  }
+  // Function to toggle the menu
+document.getElementById("toggle-btn").addEventListener("click", function() {
+    const navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("show");
+  });
+  
